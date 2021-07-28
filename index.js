ba2adf55f53d93667e593606cdeaad3a5a72d290
Tamal Das <38829310+tdas-kolkata@users.noreply.github.com>
2021-07-28 23:12:09 +0530

    added the resume route changed the anchor link to resume route in index.html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 // var cors = require('cors')
 const path = require("path");
-const  {createPool,getPool} = require('./Database/dataSource')
+const { createPool, getPool } = require('./Database/dataSource')
 require('dotenv').config();
 const PORT = process.env.PORT;
 
@@ -16,14 +16,14 @@ console.log(`Connected to DB and connection pool is created - ${isConnectedToDB}
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/ping',async (req,res)=>{
+app.get('/ping', async (req, res) => {
 	console.log(req.body);
-	res.status(200).json({"msg": "server alive"});
+	res.status(200).json({ "msg": "server alive" });
 })
 
-app.get('/pingdatabase',async (req,res)=>{
+app.get('/pingdatabase', async (req, res) => {
 	const pool = await getPool();
 	// console.log(pool);
 	const client = await pool.connect()
@@ -32,17 +32,20 @@ app.get('/pingdatabase',async (req,res)=>{
 	res.status(200).json(data.rows);
 })
 
-app.get('/vaccineInfo',(req,res)=>{
+app.get('/vaccineInfo', (req, res) => {
 	res.status(200).sendFile(path.join(__dirname, "/public/vaccine.html"))
 });
 
-app.get('/home',(req,res)=>{
+app.get('/home', (req, res) => {
 	res.status(200).sendFile(path.join(__dirname, "/public/index.html"))
 })
 
-app.use('/meeting',require('./Routes/meetingRouter'));
-
-app.listen(PORT,()=>{
-	console.log(`App is running on port ${PORT}`);
+app.get('/resume', (req, res) => {
+	res.status(200).sendFile(path.join(__dirname, "/public/Resume_Web_Dev_V1.pdf"))
 })
 
+app.use('/meeting', require('./Routes/meetingRouter'));
+
+app.listen(PORT, () => {
+	console.log(`App is running on port ${PORT}`);
+})
